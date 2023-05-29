0fb3418bb4efe8d2d2a9911211dcd22193ad4661
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-29 12:07:35 -0700

    add AccountController with verification methods.

---
+++ CsharpTemplate/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using [ProjectNameWithoutBracks].Models;
+using System.Threading.Tasks;
+using [ProjectNameWithoutBracks].ViewModels;
+
+namespace [ProjectNameWithoutBracks].Controllers
+{
+  public class AccountController : Controller
+  {
+    private readonly [ProjectNameWithoutBracks]Context _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, [ProjectNameWithoutBracks]Context db)
+    {
+      _userManager = userManager;
+      _signInManager = signInManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Register()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Register (RegisterViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(model);
+      }
+      else
+      {
+        ApplicationUser user = new ApplicationUser { UserName = model.Email };
+        IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index");
+        }
+        else
+        {
+          foreach (IdentityError error in result.Errors)
+          {
+            ModelState.AddModelError("", error.Description);
+          }
+          return View(model);
+        }
+      }
+    }
+  }
+}
